f368a865deb32e1b6f40d508887b95a48c947b2f
Nico Castañeda <nickcar13@gmail.com>
2020-05-04 13:10:37 -0500

    Manejo de estado

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,10 +1,25 @@
 import React from "react";
 
 export class BadgeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: "",
+    };
+  }
+
   handleChange = (e) => {
-    console.log({
+    /* console.log({
       name: e.target.name,
       value: e.target.value,
+    }); */
+
+    this.setState({
+      [e.target.name]: e.target.value,
     });
   };
   handleClick = (e) => {
@@ -14,6 +29,7 @@ export class BadgeForm extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     console.log("From was submitted");
+    console.log(this.state);
   };
 
   render() {
@@ -23,12 +39,57 @@ export class BadgeForm extends React.Component {
 
         <form onSubmit={this.handleSubmit}>
           <div className="font-group">
-            <label>Firts Name</label>
+            <label>First Name</label>
             <input
               onChange={this.handleChange}
               className="form-control"
               type="text"
-              name="firstname"
+              name="firstName"
+              value={this.state.firstName}
+            />
+          </div>
+
+          <div className="font-group">
+            <label>Last name</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+            />
+          </div>
+
+          <div className="font-group">
+            <label>Email</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="email"
+              name="email"
+              value={this.state.email}
+            />
+          </div>
+
+          <div className="font-group">
+            <label>Job Title</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="jobTitle"
+              value={this.state.jobTitle}
+            />
+          </div>
+
+          <div className="font-group">
+            <label>Twitter</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="twitter"
+              value={this.state.twitter}
             />
           </div>
 
