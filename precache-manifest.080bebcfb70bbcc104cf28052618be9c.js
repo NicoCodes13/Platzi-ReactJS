e5313c46d8d87c841bb2078bfa2f13d30eb1537b
Nico Castañeda <nickcar13@gmail.com>
2020-10-17 17:16:09 -0500

    Updates

---
+++ precache-manifest.080bebcfb70bbcc104cf28052618be9c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8566763c1c2366425ce",
+    "url": "/Platzi-ReactJS/static/css/main.9fe4c477.chunk.css"
+  },
+  {
+    "revision": "e8566763c1c2366425ce",
+    "url": "/Platzi-ReactJS/static/js/main.e8566763.chunk.js"
+  },
+  {
+    "revision": "d6494752099c61b85643",
+    "url": "/Platzi-ReactJS/static/js/runtime~main.d6494752.js"
+  },
+  {
+    "revision": "0030764f7905d745011a",
+    "url": "/Platzi-ReactJS/static/css/2.9f5c69df.chunk.css"
+  },
+  {
+    "revision": "0030764f7905d745011a",
+    "url": "/Platzi-ReactJS/static/js/2.0030764f.chunk.js"
+  },
+  {
+    "revision": "5d084ae231712297f9df8338390db40b",
+    "url": "/Platzi-ReactJS/static/media/platziconf-logo.5d084ae2.svg"
+  },
+  {
+    "revision": "b9eceed1254323eaf6a19cdc58716387",
+    "url": "/Platzi-ReactJS/static/media/badge-header.b9eceed1.svg"
+  },
+  {
+    "revision": "564b94db3e7d97aabf4139adb8482f49",
+    "url": "/Platzi-ReactJS/static/media/logo.564b94db.svg"
+  },
+  {
+    "revision": "8157ba13e8ac83570b073a7bee54dc53",
+    "url": "/Platzi-ReactJS/static/media/astronauts.8157ba13.svg"
+  },
+  {
+    "revision": "414bef6ec528dbb73be1fe0858fab93f",
+    "url": "/Platzi-ReactJS/static/media/stars.414bef6e.svg"
+  },
+  {
+    "revision": "b96680daf787acf888017a934b0fba8b",
+    "url": "/Platzi-ReactJS/index.html"
+  }
+];
